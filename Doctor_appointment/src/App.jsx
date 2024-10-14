d9cdb7b047bc18890d02b53300f2c0980919757b
Rajesh <rajesh798143@gmail.com>
2024-10-14 07:38:28 +0530

    Created a navbar Section

--- Doctor_appointment/src/App.jsx
+++ Doctor_appointment/src/App.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Doctors from './pages/Doctors';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import MyAppointment from './pages/MyAppointment';
+import MyProfile from './pages/MyProfile';
+import Appointment from './pages/Appointment';
+import NavBar from './components/NavBar';
 
-const App = () => {
-  return (
-    <div>
+const App = () =>{
       
-    </div>
-  );
+     return(
+      <div className='mx-4 sm:mx-[10%]'>
+
+        <NavBar/>
+
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/doctors' element={<Doctors/>} />
+          <Route path='/doctors/:specialist' element={<Doctors/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/myprofile' element={<MyProfile/>} />
+          <Route path='/myappointment' element={<MyAppointment/>} />
+          <Route path='/appointment/:docId' element={<Appointment/>} />
+          
+        </Routes>
+
+      </div>
+     )
+
 }
 
-export default App;
+export default App
---
+++ Doctor_appointment/src/Components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { assets } from '../assets/assets/assets_frontend/assets';
+
+const NavBar = () => {
+    const navigate = useNavigate()
+    const [showMenu,setShowMenu] = useState(false)
+    const [token,setToken] = useState(true)
+
+  return (
+    <div className='flex items-center justify-between text-m py-4 mb-5 border-b border-b-violet-500 '>
+       <img className='w-44 cursor-pointer' src={assets.logo} alt=''/>
+       <ul className='hidden md:flex items-start gap-5 font-medium'>
+        <NavLink to='/'>
+            <li className='py-1'>Home</li>
+            <hr className='border-none outline-none h-1 bg-primary w-3/5 m-auto hidden'/>
+        </NavLink>
+        <NavLink to='/Doctors'>
+            <li className='py-1'>All Doctors</li>
+            <hr className='border-none outline-none h-1 bg-primary w-3/5 m-auto hidden'/>
+        </NavLink>
+        <NavLink to='/Contact'>
+            <li className='py-1'>Contact</li>
+            <hr  className='border-none outline-none h-1 bg-primary w-3/5 m-auto hidden'/>
+        </NavLink>
+        <NavLink to='/About'>
+            <li className='py-1'>About</li>
+            <hr  className='border-none outline-none h-1 bg-primary w-3/5 m-auto hidden'/>
+        </NavLink>
+       </ul>
+       <div className='flex items-center gap-4'>
+        {
+            token
+            ? <div className='flex items-center cursor-pointer gap-3 group-relative'>
+               <img className='w-8 rounded-full ' src={assets.profile_pic}/>
+               <img className='w-3 rounded-full' src={assets.dropdown_icon}/>
+               <div className='absolute top-0 right-0 pt-4 text-base font-medium text-gray-600 z-20 mt-9  group-hover:block'>
+                   <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-2'>
+                      <p onClick={()=>navigate('/my-Profile')} className='hover:text-black cursor-pointer font-sm'>My Profile</p>
+                      <p onClick={()=>navigate('/my-Appointment')} className='hover:text-black cursor-pointer font-sm'>My Appointments</p>
+                      <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer font-sm'>Logout</p>
+                   </div>
+               </div>
+              </div>
+              :<button onClickCapture={()=>setToken(true)} onClick={()=>navigate('/Login')} className='bg-primary py-3 px-4 font-light text-white rounded-full flex items-center' >Create account</button>
+        }
+       </div>
+       
+    </div>
+  );
+}
+
+export default NavBar;
